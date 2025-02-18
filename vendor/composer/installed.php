85e1c172cbb8e0d059a2f0f4bea2e106b5155517
mrosyidi <rosyidiyuekunchen@gmail.com>
2025-02-18 21:58:19 +0700

    composer update

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'cafetaria/php-composer-cafetaria',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'b25cfc01b69eb6f2dd84c8ff1947f7c96ddf9b97',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'cafetaria/php-composer-cafetaria' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'b25cfc01b69eb6f2dd84c8ff1947f7c96ddf9b97',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
