313f8c9a21a1ca8b7aba1393a34bca8f99e591b6
mrosyidi <rosyidiyuekunchen@gmail.com>
2025-03-01 01:11:40 +0700

    update FoodRepositoryImpl.php menambah makanan

--- app/Repository/FoodRepositoryImpl.php
+++ app/Repository/FoodRepositoryImpl.php
@@ -34,7 +34,9 @@
 
         public function save(Food $food): void
         {
-
+            $sql = "INSERT INTO foods(name,price) VALUES(?,?)";
+            $statement = $this->connection->prepare($sql);
+            $statement->execute([$food->getName(),$food->getPrice()]);
         }
 
         public function remove(string $name): bool
