54842dc380fd7bcccb88e309a758846714b2032c
mrosyidi <rosyidiyuekunchen@gmail.com>
2025-03-01 01:14:15 +0700

    update FoodRepositoryTest.php menambah makanan

--- app/Test/FoodRepositoryTest.php
+++ app/Test/FoodRepositoryTest.php
@@ -3,6 +3,7 @@
     require_once __DIR__ . "/../../vendor/autoload.php";
 
     use \Cafetaria\Config\Database;
+    use \Cafetaria\Entity\Food;
     use \Cafetaria\Repository\FoodRepositoryImpl;
 
     function testFindAll(): void 
@@ -12,5 +13,15 @@
         $food = $foodRepository->findAll();
         var_dump($food);
     }
+
+    function testSave(): void 
+    {
+        $food = new Food("Ayam Panggang", 15000);
+        $connection = Database::getConnection();
+        $foodRepository = new FoodRepositoryImpl($connection);
+        $foodRepository->save($food);
+        $foods = $foodRepository->findAll();
+        var_dump($foods);
+    }
     
-    testFindAll();
+    testSave();
