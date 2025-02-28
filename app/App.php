b2cc0400b5eb5f60cf08809bd3d277569e5fb83b
mrosyidi <rosyidiyuekunchen@gmail.com>
2025-02-28 19:47:14 +0700

    update App.php use FoodView.php

--- app/App.php
+++ app/App.php
@@ -5,12 +5,14 @@
     use \Cafetaria\Config\Database;
     use \Cafetaria\Repository\FoodRepositoryImpl;
     use \Cafetaria\Service\FoodServiceImpl;
+    use \Cafetaria\View\FoodView;
     use \Cafetaria\Helper\InputHelper;
 
     $connection = Database::getConnection();
 
     $foodRepository = new FoodRepositoryImpl($connection);
     $foodService = new FoodServiceImpl($foodRepository);
+    $foodView = new FoodView($foodService);
 
     echo "Cafetaria App" . PHP_EOL;
 
