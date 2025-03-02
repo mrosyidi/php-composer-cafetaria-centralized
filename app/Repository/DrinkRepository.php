0d1c608050075454867b899e40c4efea82c90b24
mrosyidi <rosyidiyuekunchen@gmail.com>
2025-03-02 22:09:08 +0700

    add DrinkRepository.php

---
+++ app/Repository/DrinkRepository.php
@@ -0,0 +1,12 @@
+<?php 
+
+    namespace Cafetaria\Repository;
+
+    use \Cafetaria\Entity\Drink;
+
+    interface DrinkRepository 
+    {
+        public function findAll(): array;
+        public function save(Drink $drink): void;
+        public function remove(string $name): bool;
+    }
