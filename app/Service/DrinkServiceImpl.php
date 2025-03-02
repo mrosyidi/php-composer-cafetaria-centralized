25c5d98598846a8039fcb18d842fa85448dd0840
mrosyidi <rosyidiyuekunchen@gmail.com>
2025-03-02 22:43:21 +0700

    add DrinkServiceImpl.php

---
+++ app/Service/DrinkServiceImpl.php
@@ -0,0 +1,51 @@
+<?php 
+
+    namespace Cafetaria\Service;
+
+    use \Cafetaria\Entity\Drink;
+    use \Cafetaria\Repository\DrinkRepository;
+    use \Cafetaria\Service\DrinkService;
+    
+    class DrinkServiceImpl implements DrinkService 
+    {
+        private DrinkRepository $drinkRepository;
+
+        public function __construct(DrinkRepository $drinkRepository)
+        {
+            $this->drinkRepository = $drinkRepository;
+        }
+
+        public function showDrink(): void
+        {
+            echo "DAFTAR MINUMAN" . PHP_EOL;
+
+            $drinks = $this->drinkRepository->findAll();
+
+            if(empty($drinks))
+            {
+                echo "Tidak ada daftar minuman" . PHP_EOL;
+            }else
+            {
+                foreach($drinks as $number => $drink)
+                {
+                    $number++;
+                    echo "$number. " . $drink->getName() . "  Rp." . $drink->getPrice() . PHP_EOL;
+                }
+            }
+        }
+
+        public function addDrink(string $name, int $price): void
+        {
+
+        }
+
+        public function getDrink(): array
+        {
+
+        }
+
+        public function removeDrink(string $name): bool
+        {
+
+        }
+    }
