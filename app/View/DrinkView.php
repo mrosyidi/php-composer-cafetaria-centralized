07f4df606a0350fdfcf1c3da5711b14fecc7deb7
mrosyidi <rosyidiyuekunchen@gmail.com>
2025-03-04 00:05:15 +0700

    add DrinkView.php

---
+++ app/View/DrinkView.php
@@ -0,0 +1,45 @@
+<?php 
+
+    namespace Cafetaria\View;
+
+    use \Cafetaria\Service\DrinkService;
+    use \Cafetaria\Helper\InputHelper;
+
+    class DrinkView 
+    {
+        private DrinkService $drinkService;
+
+        public function __construct(DrinkService $drinkService)
+        {
+            $this->drinkService = $drinkService;
+        }
+
+        public function showDrink(): void 
+        {
+            while(true)
+            {
+                $this->drinkService->showDrink();
+
+                echo "Menu Minuman" . PHP_EOL;
+                echo "1. Tambah Minuman" . PHP_EOL;
+                echo "2. Hapus Minuman" . PHP_EOL;
+                echo "x. Kembali" . PHP_EOL;
+
+                $pilihan = InputHelper::input("Pilih");
+
+                if($pilihan == "1")
+                {
+
+                }else if($pilihan == "2")
+                {
+
+                }else if($pilihan == "x")
+                {
+                    break;
+                }else 
+                {
+                    echo "Pilihan tidak dimengerti" . PHP_EOL;
+                }
+            }
+        }
+    }
