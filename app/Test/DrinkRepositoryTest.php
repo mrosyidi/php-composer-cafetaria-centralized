de2b4bd3c6e3f2e3b167dd4039e3cb40ea09e41c
mrosyidi <rosyidiyuekunchen@gmail.com>
2025-03-05 01:15:51 +0700

    update DrinkRepositoryTest.php menghapus minuman

--- app/Test/DrinkRepositoryTest.php
+++ app/Test/DrinkRepositoryTest.php
@@ -24,4 +24,14 @@
         var_dump($drinks);
     }
 
-    testSave();
+    function testRemove(): void 
+    {
+        $connection = Database::getConnection();
+        $drinkRepository = new DrinkRepositoryImpl($connection);
+        $result = $drinkRepository->remove("Es Campur");
+        var_dump($result);
+        $drinks = $drinkRepository->findAll();
+        var_dump($drinks);
+    }
+
+    testRemove();
