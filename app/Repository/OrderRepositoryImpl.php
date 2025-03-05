6e853d36b1a8e98a92983c82baae8270e04005b2
mrosyidi <rosyidiyuekunchen@gmail.com>
2025-03-06 01:02:44 +0700

    add OrderRepositoryImpl.php

---
+++ app/Repository/OrderRepositoryImpl.php
@@ -0,0 +1,47 @@
+<?php 
+
+    namespace Cafetaria\Repository;
+
+    use \Cafetaria\Entity\Order;
+    use \Cafetaria\Repository\OrderRepository;
+
+    class OrderRepositoryImpl implements OrderRepository 
+    {
+        private \PDO $connection;
+
+        public function __construct(\PDO $connection)
+        {
+            $this->connection = $connection;
+        }
+
+        public function findAll(): array 
+        {
+            $sql = "SELECT code,name,price,qty FROM orders";
+            $statement = $this->connection->prepare($sql);
+            $statement->execute();
+            $orders = [];
+
+            foreach($statement as $row)
+            {
+                $order = new order();
+                $order->setCode($row['code']);
+                $order->setName($row['name']);
+                $order->setPrice($row['price']);
+                $order->setQty($row['qty']);
+                $order->setSubTotal();
+                $orders[] = $order;
+            }
+
+            return $orders;
+        }
+
+        public function save(Order $order): void 
+        {
+            
+        }
+
+        public function remove(int $code): void 
+        {
+            
+        }
+    }
