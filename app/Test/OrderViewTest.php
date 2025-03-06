a2fda3db2f0a45fb42e3170bad463667fe08a949
mrosyidi <rosyidiyuekunchen@gmail.com>
2025-03-06 18:35:52 +0700

    add OrderViewTest.php

---
+++ app/Test/OrderViewTest.php
@@ -0,0 +1,19 @@
+<?php 
+
+    require_once __DIR__ . '/../../vendor/autoload.php';
+
+    use \Cafetaria\Config\Database;
+    use \Cafetaria\Repository\OrderRepositoryImpl;
+    use \Cafetaria\Service\OrderServiceImpl;
+    use \Cafetaria\View\OrderView;
+
+    function testViewShowOrder(): void 
+    {
+        $connection = Database::getConnection();
+        $orderRepository = new OrderRepositoryImpl($connection);
+        $orderService = new OrderServiceImpl($orderRepository);
+        $orderView = new OrderView($orderService);
+        $orderView->showOrder();
+    }
+
+    testViewShowOrder();
