0c9902792fbf886ad145054ccd9fc8c97671745b
mrosyidi <rosyidiyuekunchen@gmail.com>
2025-03-08 22:14:29 +0700

    update OrderViewTest.php menambah pemesanan makanan kode sama

--- app/Test/OrderViewTest.php
+++ app/Test/OrderViewTest.php
@@ -3,6 +3,10 @@
     require_once __DIR__ . '/../../vendor/autoload.php';
 
     use \Cafetaria\Config\Database;
+    use \Cafetaria\Repository\FoodRepositoryImpl;
+    use \Cafetaria\Service\FoodServiceImpl;
+    use \Cafetaria\Repository\DrinkRepositoryImpl;
+    use \Cafetaria\Service\DrinkServiceImpl;
     use \Cafetaria\Repository\OrderRepositoryImpl;
     use \Cafetaria\Service\OrderServiceImpl;
     use \Cafetaria\View\OrderView;
@@ -16,4 +20,18 @@
         $orderView->showOrder();
     }
 
-    testViewShowOrder();
+    function testViewAddOrder(): void 
+    {
+        $connection = Database::getConnection();
+        $foodRepository = new FoodRepositoryImpl($connection);
+        $foodService = new FoodServiceImpl($foodRepository);
+        $drinkRepository = new DrinkRepositoryImpl($connection);
+        $drinkService = new DrinkServiceImpl($drinkRepository);
+        $orderRepository = new OrderRepositoryImpl($connection);
+        $orderService = new OrderServiceImpl($orderRepository);
+        $orderView = new OrderView($foodService, $drinkService, $orderService);
+        $orderView->addOrder(1, false);
+        $orderService->showOrder();
+    }
+
+    testViewAddOrder();
