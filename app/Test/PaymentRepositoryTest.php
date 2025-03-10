d8d3a97a6ed7afa916f8dc87023049c518e367b8
mrosyidi <rosyidiyuekunchen@gmail.com>
2025-03-10 22:31:29 +0700

    update PaymentRepositoryTest.php menambah data pembayaran

--- app/Test/PaymentRepositoryTest.php
+++ app/Test/PaymentRepositoryTest.php
@@ -3,6 +3,7 @@
     require_once __DIR__ . '/../../vendor/autoload.php';
 
     use \Cafetaria\Config\Database;
+    use \Cafetaria\Entity\Payment;
     use \Cafetaria\Repository\PaymentRepositoryImpl;
 
     function testFindAll(): void 
@@ -13,4 +14,14 @@
         var_dump($payments);
     }
 
-    testFindAll();
+    function testSave(): void 
+    {
+        $connection = Database::getConnection();
+        $payment = new Payment(2,45000,50000,5000);
+        $paymentRepository = new PaymentRepositoryImpl($connection);
+        $paymentRepository->save($payment);
+        $payments = $paymentRepository->findAll();
+        var_dump($payments);
+    }
+
+    testSave();
