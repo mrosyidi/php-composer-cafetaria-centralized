bb86b37e3f5fc2fc31a8640a8b2a338647d12375
mrosyidi <rosyidiyuekunchen@gmail.com>
2025-03-10 22:53:58 +0700

    update PaymentViewTest.php menambah data pembayaran

--- app/Test/PaymentViewTest.php
+++ app/Test/PaymentViewTest.php
@@ -20,4 +20,16 @@
         $paymentView->showPayment();
     }
 
-    testViewShowPayment();
+    function testViewAddPayment(): void
+    {
+        $connection = Database::getConnection();
+        $orderRepository = new OrderRepositoryImpl($connection);
+        $paymentRepository = new PaymentRepositoryImpl($connection);
+        $orderService = new OrderServiceImpl($orderRepository);
+        $paymentService = new PaymentServiceImpl($paymentRepository);
+        $paymentView = new PaymentView($orderService, $paymentService);
+        $orderService->showOrder();
+        $paymentView->addPayment();
+    }
+
+    testViewAddPayment();
