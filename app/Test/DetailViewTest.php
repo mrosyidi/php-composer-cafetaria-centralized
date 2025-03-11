9580e602221b7de3448fb7cc7a49e0bad006ffaf
mrosyidi <rosyidiyuekunchen@gmail.com>
2025-03-11 20:26:04 +0700

    add DetailViewTest.php

---
+++ app/Test/DetailViewTest.php
@@ -0,0 +1,23 @@
+<?php   
+
+    require_once __DIR__ . "/../../vendor/autoload.php";
+
+    use \Cafetaria\Config\Database;
+    use \Cafetaria\Repository\PaymentRepositoryImpl;
+    use \Cafetaria\Repository\DetailRepositoryImpl;
+    use \Cafetaria\Service\PaymentServiceImpl;
+    use \Cafetaria\Service\DetailServiceImpl;
+    use \Cafetaria\View\DetailView;
+
+    function testViewShowDetail(): void 
+    {
+        $connection = Database::getConnection();
+        $paymentRepository = new PaymentRepositoryImpl($connection);
+        $paymentService = new PaymentServiceImpl($paymentRepository);
+        $detailReposiory = new DetailRepositoryImpl($connection);
+        $detailService = new DetailServiceImpl($detailReposiory);
+        $detailView = new DetailView($detailService, $paymentService);
+        $detailView->showDetail();
+    }
+
+    testViewShowDetail();
