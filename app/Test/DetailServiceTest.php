ceaa9d3f403179c0a4439087a093eb73fcb2439d
mrosyidi <rosyidiyuekunchen@gmail.com>
2025-03-12 22:21:27 +0700

    update DetailServiceTest.php menambah data detail

--- app/Test/DetailServiceTest.php
+++ app/Test/DetailServiceTest.php
@@ -3,6 +3,7 @@
     require_once __DIR__ . "/../../vendor/autoload.php";
 
     use \Cafetaria\Config\Database;
+    use \Cafetaria\Repository\OrderRepositoryImpl;
     use \Cafetaria\Repository\DetailRepositoryImpl;
     use \Cafetaria\Service\DetailServiceImpl;
 
@@ -14,4 +15,15 @@
         $detailService->showDetail(2);
     }
     
-    testShowDetail();
+    function testAddDetail(): void 
+    {
+        $connection = Database::getConnection();
+        $orderRepository = new OrderRepositoryImpl($connection);
+        $detailRepository = new DetailRepositoryImpl($connection);
+        $detailService = new DetailServiceImpl($detailRepository);
+        $orders = $orderRepository->findAll();
+        $detailService->addDetail($orders);
+        $detailService->showDetail(2);
+    }
+
+    testAddDetail();
