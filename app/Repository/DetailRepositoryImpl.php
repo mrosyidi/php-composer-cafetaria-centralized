6624054c56ef87525a9a8cbd780b7a7eee8f4beb
mrosyidi <rosyidiyuekunchen@gmail.com>
2025-03-12 22:08:30 +0700

    update DetailRepositoryImpl.php menambah data detail

--- app/Repository/DetailRepositoryImpl.php
+++ app/Repository/DetailRepositoryImpl.php
@@ -36,6 +36,9 @@
 
         public function save(Detail $detail): void
         {
-            
+            $sql = "INSERT INTO details(code,name,price,qty,sub_total) VALUES(?,?,?,?,?)";
+            $statement = $this->connection->prepare($sql);
+            $statement->execute([$detail->getCode(),$detail->getName(),
+            $detail->getPrice(),$detail->getQty(),$detail->getSubTotal()]);
         }
     }
