37a95bc95d1457ff4e0424a0712b2901d42396d7
mrosyidi <rosyidiyuekunchen@gmail.com>
2025-03-13 22:41:55 +0700

    update PaymentView.php menambah detail pesanan dari data yang sudah diproses

--- app/View/PaymentView.php
+++ app/View/PaymentView.php
@@ -4,6 +4,7 @@
 
     use \Cafetaria\Service\OrderService;
     use \Cafetaria\Service\PaymentService;
+    use \Cafetaria\Service\DetailService;
     use \Cafetaria\Helper\FindHelper;
     use \Cafetaria\Helper\DuplicateHelper;
     use \Cafetaria\Helper\InputHelper;
@@ -13,11 +14,13 @@
     {
         private OrderService $orderService;
         private PaymentService $paymentService;
+        private DetailService $detailService;
 
-        public function __construct(OrderService $orderService, PaymentService $paymentService)
+        public function __construct(OrderService $orderService, PaymentService $paymentService, DetailService $detailService)
         {
             $this->orderService = $orderService;
             $this->paymentService = $paymentService;
+            $this->detailService = $detailService;
         }
 
         public function showPayment(): void
@@ -80,6 +83,7 @@
                         $elements = DuplicateHelper::duplicate($orders, $code);
                         $this->paymentService->addPayment($code, $pay, $money);
                         $this->orderService->removeOrder($code);
+                        $this->detailService->addDetail($elements);
 
                         echo "Kembalian : Rp." . $change . PHP_EOL;
                     }
